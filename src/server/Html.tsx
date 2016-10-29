b6f4f1b2a16e36647337f5a736986b122775c6a3
ywarezk <ywarezk@gmail.com>
2016-10-29 12:11:59 +0300

    tsc fix for html.tsx

--- src/server/Html.tsx
+++ src/server/Html.tsx
@@ -7,9 +7,8 @@
  * @copyright: Nerdeez Ltd
  */
 
-import React from 'react';
-import Helmet from 'react-helmet';
-import * as serialize from 'serialize-javascript';
+import * as React from 'react';
+import * as Helmet from 'react-helmet';
 declare var global : any;
 
 interface HtmlPropTypes {
@@ -36,7 +35,7 @@ export default class Html extends React.Component<HtmlPropTypes, any> {
                     <link type="text/css" rel="stylesheet" href={assets.app.css} />
                 </head>
                 <body>
-                    <script dangerouslySetInnerHTML={{ __html: `window.__initialState=${serialize(store.getState())};${globals}` }} charSet="UTF-8" />
+                    <script dangerouslySetInnerHTML={{ __html: `window.__initialState=${JSON.stringify(store.getState())};${globals}` }} charSet="UTF-8" />
                     <div id="nz-content" dangerouslySetInnerHTML={{ __html: component }} />
                     <script src={assets.app.js} />
                 </body>
