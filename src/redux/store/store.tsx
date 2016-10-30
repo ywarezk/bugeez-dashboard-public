95fc551e3ddbd0b4c6a67f56b3f2d498efb895d5
ywarezk <ywarezk@gmail.com>
2016-10-30 19:04:56 +0200

    store.tsx now passing lint

--- src/redux/store/store.tsx
+++ src/redux/store/store.tsx
@@ -7,41 +7,64 @@
  * @copyright: Nerdeez Ltd
  */
 
-import { createStore, applyMiddleware, compose } from "redux";
-import { routerMiddleware } from "react-router-redux";
-import { createEpicMiddleware, combineEpics } from "redux-observable";
-import thunk from "redux-thunk";
-import { persistState } from "redux-devtools";
-import reducer from "../reducers/reducers.tsx";
-import { getAllTasks, searchTasks } from "../epics/todo.tsx";
-import DevTools from "../../components/DevTools/DevTools.tsx";
+/*=======================
+ * begin imports
+ *=======================*/
 
-declare var __DEVELOPMENT__: any;
-declare var __CLIENT__: any;
+import * as H from 'history';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import { persistState } from 'redux-devtools';
+import { createEpicMiddleware, combineEpics } from 'redux-observable';
+import reduxThunk from 'redux-thunk';
+import reducers from '../reducers/reducers.tsx';
+import { getAllTasks, searchTasks } from '../epics/todo.tsx';
+import DevTools from '../../components/DevTools/DevTools.tsx';
 
-export function nzCreateStore(history = null) {
+/*=======================
+ * end imports
+ *=======================*/
+
+/*=======================
+ * begin declarations
+ *=======================*/
+
+declare var __DEVELOPMENT__: boolean;
+declare var __CLIENT__: boolean;
+interface IWindow {
+    __initialState: Object;
+    location: {href : {match() : boolean}};
+    devToolsExtension?() : Object;
+}
+declare var window : IWindow;
+
+/*=======================
+ * end declarations
+ *=======================*/
+
+export function store(history : H.History = null) {
     const reduxRouterMiddleware = routerMiddleware(history);
     const middleware = [
         reduxRouterMiddleware,
-        thunk as any,
-        createEpicMiddleware(combineEpics(getAllTasks, searchTasks)),
+        reduxThunk,
+        createEpicMiddleware(combineEpics(getAllTasks, searchTasks))
     ];
     let finalCreateStore;
     if (__DEVELOPMENT__ && __CLIENT__) {
         finalCreateStore = compose(
-            applyMiddleware(...middleware as any),
-            (window as any).devToolsExtension ? (window as any).devToolsExtension() : DevTools.instrument(),
-            persistState((window as any).location.href.match(/[?&]debug_session=([^&]+)\b/))
+            applyMiddleware(...middleware),
+            window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument(),
+            persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
         )(createStore);
     } else if (__CLIENT__) {
         return finalCreateStore(
-            reducer,
-            (window as any).__initialState
+            reducers,
+            window.__initialState
         );
     } else {
         finalCreateStore = compose(
             applyMiddleware(...middleware)
         )(createStore);
     }
-    return finalCreateStore(reducer);
+    return finalCreateStore(reducers);
 }
